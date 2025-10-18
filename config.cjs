10112aac9abd4f892297c4265cda0d08e74ee70d
aboba_42 <golovkobogdan181@gmail.com>
2025-10-19 00:55:47 +0500

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -1,16 +1,17 @@
 // config.cjs
 module.exports = {
   discordToken:
-    "MTQxOTIwNDg1NDA3NjY3NDExOA.GR2HO0.g1eSlCU78c3DTqYTrDnQY84Z2ZjQkUFV1u37Hc",
-  discordChannelId: "1419203577804619950", // ID канала, где пишут пользователи
-  discordStatusChannelId: "1419710406276022404", // статус
+    "yourtoken",
+  discordChannelId: "id channel", // discord channel to relay messages
+  discordStatusChannelId: "1419710406276022404", // сchannel status of server
   rcon: {
-    host: "188.127.241.201",
-    port: 28118, // порт сервера
-    password: "463719",
+    host: "188.127.241.201", //ip of server
+    port: 28118, // port
+    password: "463719", // rcon password check in server.cfg
   },
   // опции:
   prefix: "{Discord}", // не трогать
   relayBots: false, // не трогать
   ignoreCommandsStartingWith: "!", // а это похуй
 };
+
